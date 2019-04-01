7edac0f374fba30cb5ed3ccf4495b7385e79ba38
Minux <minux8905@gmail.com>
2019-04-01 23:57:02 +0900

    first commit

--- app/src/main/java/com/example/expedia03/MainActivity.java
+++ app/src/main/java/com/example/expedia03/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button b;
+    Button bdsdsdsdfdsfsfs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

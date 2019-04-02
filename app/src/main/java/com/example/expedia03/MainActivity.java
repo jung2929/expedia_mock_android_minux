88dab225735cfd71e588e5ac5385b7aca1b3f68d
Minux <minux8905@gmail.com>
2019-04-02 13:07:03 +0900

    탭아이콘/로그인or회원가입뷰 만들었고..로그인api(구글, 페북)해야됨

--- app/src/main/java/com/example/expedia03/MainActivity.java
+++ app/src/main/java/com/example/expedia03/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
@@ -14,18 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initApp();
+    }
 
+    private void initApp(){
         tabLayout = findViewById(R.id.tablayout);
-        tab1 = tabLayout.newTab();
-        tab1.setIcon(R.drawable.tab1_selected);
-        tabLayout.addTab(tab1);
-        tab2 = tabLayout.newTab();
-        tab2.setIcon(R.drawable.tab2_unselected);
-        tabLayout.addTab(tab2);
-        tab3 = tabLayout.newTab();
-        tab3.setIcon(R.drawable.tab3_unselected);
-        tabLayout.addTab(tab3);
+        tab1 = tabLayout.getTabAt(0);
+        tab2 = tabLayout.getTabAt(1);
+        tab3 = tabLayout.getTabAt(2);
+        tab1.setIcon(R.drawable.travel_booking_tab);
+        tab1.setText("여행예약");
+        tab2.setIcon(R.drawable.schedule_tab);
+        tab2.setText("일정");
+        tab3.setIcon(R.drawable.account_tab);
+        tab3.setText("계정");
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
             @Override
             public void onTabSelected(TabLayout.Tab tab){
@@ -44,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void changeTabContent(int pos){
-        FrameLayout tab1Content = findViewById(R.id.tab1_content);
+        RelativeLayout tab1Content = findViewById(R.id.tab1_content);
         LinearLayout tab2Content = findViewById(R.id.tab2_content);
         LinearLayout tab3Content = findViewById(R.id.tab3_content);
 
@@ -53,25 +61,19 @@ public class MainActivity extends AppCompatActivity {
                 tab1Content.setVisibility(View.VISIBLE);
                 tab2Content.setVisibility(View.INVISIBLE);
                 tab3Content.setVisibility(View.INVISIBLE);
-                tab1.setIcon(R.drawable.tab1_selected);
-                tab2.setIcon(R.drawable.tab2_unselected);
-                tab3.setIcon(R.drawable.tab3_unselected);
                 break;
             case 1:
                 tab1Content.setVisibility(View.INVISIBLE);
                 tab2Content.setVisibility(View.VISIBLE);
                 tab3Content.setVisibility(View.INVISIBLE);
-                tab1.setIcon(R.drawable.tab1_unselected);
-                tab2.setIcon(R.drawable.tab2_selected);
-                tab3.setIcon(R.drawable.tab3_unselected);
                 break;
             case 2:
                 tab1Content.setVisibility(View.INVISIBLE);
                 tab2Content.setVisibility(View.INVISIBLE);
                 tab3Content.setVisibility(View.VISIBLE);
-                tab1.setIcon(R.drawable.tab1_unselected);
+                /*tab1.setIcon(R.drawable.tab1_unselected);
                 tab2.setIcon(R.drawable.tab2_unselected);
-                tab3.setIcon(R.drawable.tab3_selected);
+                tab3.setIcon(R.drawable.tab3_selected);*/
                 break;
         }
     }
